20b5b5be3ec3a78e42bf7684b5a482c5b64ebd28
Vikas_Android_Engineer <vicay0001@gmail.com>
2022-10-01 17:01:44 +0530

    Add name by text field

--- ButtonClickApp/app/src/main/java/com/learningacademy/buttonclickapp/MainActivity.kt
+++ ButtonClickApp/app/src/main/java/com/learningacademy/buttonclickapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.learningacademy.buttonclickapp
 
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,31 +9,24 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    private var userInput: EditText?=null
-    private var button: Button?=null
-    private var textView: TextView?=null
-    private var numTimesClicked = 0
+    private var userInput: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // R class files have all information about resources
         userInput = findViewById(R.id.editTextTextPersonName)
+        userInput?.setText("")
         button = findViewById(R.id.button)
         textView = findViewById(R.id.textView)
         textView?.movementMethod = ScrollingMovementMethod()
 
-        button?.setOnClickListener(object: View.OnClickListener{
-            override fun onClick(v: View?) {
-                numTimesClicked += 1
-                if(numTimesClicked != 1){
-                    textView?.append("The Button got tapped $numTimesClicked time")
-                    textView?.append("s\n")
-                }else{
-                    textView?.append("The Button got tapped $numTimesClicked time\n")
-                }
-
-
-            }
-        }) }
+        button?.setOnClickListener {
+            textView?.append(userInput?.text)
+            textView?.append("\n")
+            userInput?.text?.clear()
+        }
     }
+}
