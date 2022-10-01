1d74647221bca361b2ace2892a58f58a752d758b
Vikas_Android_Engineer <vicay0001@gmail.com>
2022-10-01 23:54:45 +0530

    activity lifecycle explained

--- ButtonClickApp/app/src/main/java/com/learningacademy/buttonclickapp/MainActivity.kt
+++ ButtonClickApp/app/src/main/java/com/learningacademy/buttonclickapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.learningacademy.buttonclickapp
 
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private var userInput: EditText? = null
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate : Called")
         setContentView(R.layout.activity_main)
         // R class files have all information about resources
         userInput = findViewById(R.id.editTextTextPersonName)
@@ -24,9 +27,51 @@ class MainActivity : AppCompatActivity() {
         textView?.movementMethod = ScrollingMovementMethod()
 
         button?.setOnClickListener {
+            Log.d(TAG, "onClick : Called")
             textView?.append(userInput?.text)
             textView?.append("\n")
             userInput?.text?.clear()
         }
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "onRestore : Called")
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "onRestart : Called")
+        super.onRestart()
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart : Called")
+        super.onStart()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause : Called")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG, "onSaveInstanceState : Called")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume : Called")
+        super.onResume()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop : Called")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy : Called")
+        super.onDestroy()
     }
 }
