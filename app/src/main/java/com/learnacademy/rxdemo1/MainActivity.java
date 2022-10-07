217d62fd40568eccacbf06091433b882086fc0bd
Vikas_Android_Engineer <vicay0001@gmail.com>
2022-10-07 10:05:01 +0530

    Composite Disposable Added

--- app/src/main/java/com/learnacademy/rxdemo1/MainActivity.java
+++ app/src/main/java/com/learnacademy/rxdemo1/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.internal.schedulers.SchedulerMultiWorkerSupport;
 import io.reactivex.rxjava3.observers.DisposableObserver;
@@ -17,10 +19,11 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG= "myApp";
-    private String greeting= "Hello From RxJava";
+    private final String greeting= "Hello From RxJava";
     private Observable<String> myObservable;
-//    private Observer<String> myObserver;
     private DisposableObserver<String> myObserver;
+    private DisposableObserver<String> myObserver2;
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
 
 
     private TextView textView;
@@ -34,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         myObservable.subscribeOn(Schedulers.io());         // Rx java make a new thread to do this task on IO thread .
         myObservable.observeOn(AndroidSchedulers.mainThread());  // observable data stream will observe on main thread With the help of AndroidSchedulers
 
-        myObserver=new DisposableObserver<String>() {
+        myObserver = new DisposableObserver<String>() {
             @Override
             public void onNext(String s) {
                 Log.i(TAG," onNext invoked");
@@ -45,22 +48,45 @@ public class MainActivity extends AppCompatActivity {
             public void onError(Throwable e) {
                 Log.i(TAG," onError invoked");
             }
-
-
             @Override
             public void onComplete() {
                 Log.i(TAG," onComplete invoked");
             }
         };
-
+        compositeDisposable.add(myObserver);
         myObservable.subscribe(myObserver);
 
-    }
+
+    myObserver2 = new DisposableObserver<String>() {
+        @Override
+        public void onNext(String s) {
+            Log.i(TAG," onNext invoked");
+            Toast.makeText(getApplicationContext(),""+s,Toast.LENGTH_LONG).show();
+            textView.setText(s);
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            Log.i(TAG," onError invoked");
+        }
+
+
+        @Override
+        public void onComplete() {
+            Log.i(TAG," onComplete invoked");
+        }
+    };
+        compositeDisposable.add(myObserver2);
+        myObservable.subscribe(myObserver2);
+
+}
 
     @Override
     public void onDestroy(){
         super.onDestroy();
-       myObserver.dispose();
+//       myObserver.dispose();
+//       myObserver2.dispose();
+        compositeDisposable.clear();
     }
 
 }
