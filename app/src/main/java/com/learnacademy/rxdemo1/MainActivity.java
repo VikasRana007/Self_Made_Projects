fa9e587a01e017d6017f4c9ed635e32c5ea0f018
Vikas_Android_Engineer <vicay0001@gmail.com>
2022-10-08 09:32:43 +0530

    just operatore uses as String & Array

--- app/src/main/java/com/learnacademy/rxdemo1/MainActivity.java
+++ app/src/main/java/com/learnacademy/rxdemo1/MainActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.List;
+
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
@@ -19,89 +21,42 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG= "myApp";
-    private final String greeting= "Hello From RxJava";
+//    private String[] greetings = {"Hello A", "Hello B", "Hello C"};
     private Observable<String> myObservable;
     private DisposableObserver<String> myObserver;
-    private DisposableObserver<String> myObserver2;
-    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
 
-    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textView=findViewById(R.id.tvGreeting);
-        myObservable=Observable.just(greeting);
-
-                                                            // To initialise the observer , this is one way to initialization
-        myObservable.subscribeOn(Schedulers.io());         // Rx java make a new thread to do this task on IO thread .
-        myObservable.observeOn(AndroidSchedulers.mainThread());  // observable data stream will observe on main thread With the help of AndroidSchedulers
-
-
-
-        myObserver = new DisposableObserver<String>() {
-            @Override
-            public void onNext(String s) {
-                Log.i(TAG," onNext invoked");
-                textView.setText(s);
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.i(TAG," onError invoked");
-            }
-            @Override
-            public void onComplete() {
-                Log.i(TAG," onComplete invoked");
-            }
-        };
-
+        myObservable = Observable.just("Hello A", "Hello B", "Hello C");
         compositeDisposable.add(
-        myObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(myObserver));
+                myObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(getObserver())
+                        );
+}
 
-//        compositeDisposable.add(myObserver);       // These two lines(67,68) can also be written as above one line code using composite disposable
-//        myObservable.subscribe(myObserver);
+private DisposableObserver getObserver(){
 
-
-    myObserver2 = new DisposableObserver<String>() {
+    myObserver = new DisposableObserver<String>() {
         @Override
         public void onNext(String s) {
-            Log.i(TAG," onNext invoked");
-            Toast.makeText(getApplicationContext(),""+s,Toast.LENGTH_LONG).show();
-            textView.setText(s);
+            Log.i(TAG," onNext invoked  : "+s);
         }
 
         @Override
         public void onError(Throwable e) {
             Log.i(TAG," onError invoked");
         }
-
-
         @Override
         public void onComplete() {
-            Log.i(TAG," onComplete invoked");
+            Log.i(TAG," onComplete invoked  : ");
         }
     };
-
-        compositeDisposable.add(
-                myObservable.subscribeWith(myObserver2));
-
-       // compositeDisposable.add(myObserver2);
-      // myObservable.subscribe(myObserver2);
-
+        return myObserver;
 }
-
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-//       myObserver.dispose();
-//       myObserver2.dispose();
-        compositeDisposable.clear();
-    }
-
 }
